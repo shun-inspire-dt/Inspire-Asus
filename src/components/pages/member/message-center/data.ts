5ec526452659ccde51db70b2e6b634231b9a3dad
王政舜 <shun.wang@inspire-dt.com>
2025-11-17 16:46:52 +0800

    feat: 實作會員訊息中心頁面內容與表格佈局
    - 更新 message-center.astro 頁面
      - 引入 data 與 field 資料定義
      - 引入 Body、Title、Sort 會員頁面工具元件
      - 引入 Table 表格元件用於展示訊息列表
      - 實作頁面佈局:標題區塊顯示排序功能、表格展示訊息資料、底部排序選項

---
+++ src/components/pages/member/message-center/data.ts
@@ -0,0 +1,28 @@
+/** Type */
+import type { tableChidType, tableChidTRType } from '@components/common/tables/tables.type';
+
+export const data: tableChidTRType[] = [
+    ['未讀', '2025/09/15', '審核已通過', '您上架的解決方案 iShops 已審核通過'],
+    ['已讀', '2025/09/14', '補件通知', '您上架的解決方案 iShops 需要補件'],
+    ['已讀', '2025/09/13', '審核中', '您的解決方案 iShops 正在審核中'],
+    ['已讀', '2025/09/12', '審核未通過', '您上架的解決方案 iShops 未審核通過，請檢查您的資料'],
+    ['已讀', '2025/09/11', '已回覆', '審核團隊已回覆您的問題'],
+    ['已讀', '2025/09/10', '需進一步審核', '您的解決方案 iShops 需要更詳細的資料進行審核'],
+    ['已讀', '2025/09/09', '提交成功', '您的解決方案 iShops 已成功提交'],
+    ['已讀', '2025/09/08', '修改建議', '我們建議對您的解決方案 iShops 進行一些修改'],
+    ['已讀', '2025/09/07', '等待客戶確認', '您的解決方案 iShops 正在等待客戶的確認'],
+    ['已讀', '2025/09/06', '已上架', '您的解決方案 iShops 已成功上架到平台']
+].flatMap((x) => ({
+    items: x.flatMap((y) => ({ text: y, attribute: { style: { 'color': y === '未讀' ? 'var(--color-red-300, #B42D18)' : '' } } }))
+}));
+
+const width = {
+    狀態: '4.5rem',
+    時間: '7.375rem',
+    標題: '8rem',
+    內容: 'auto'
+};
+export const field: tableChidType[] = ['狀態', '時間', '標題', '內容'].map((x) => ({
+    text: x,
+    attribute: { style: { 'min-width': width[x as keyof typeof width] } }
+}));
