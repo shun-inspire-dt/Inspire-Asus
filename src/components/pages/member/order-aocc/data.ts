0817f806e9833ecd8de14807550d18676440933e
王政舜 <shun.wang@inspire-dt.com>
2025-11-17 16:47:34 +0800

    feat: 實作會員諮詢夾頁面內容與表格佈局及詳細頁面路由
    - 更新 order-aocc.astro 頁面
      - 引入 data 與 field 資料定義
      - 引入 Body、Title、Sort 會員頁面工具元件
      - 引入 Table 表格元件用於展示諮詢夾列表
      - 實作頁面佈局:標題區塊顯示排序功能、表格展示諮詢資料、底部排序選項
    - 更新 [...id].astro 詳細頁面
      - 引入 data 與 inqueryData 資料定義
      - 引入 Body、Title 會員頁面工具元件

---
+++ src/components/pages/member/order-aocc/data.ts
@@ -0,0 +1,37 @@
+/** Type */
+import type { tableChidType, tableChidTRType } from '@components/common/tables/tables.type';
+/** Common Component */
+import TableLink from './tableLink.astro';
+
+export const data: tableChidTRType[] = [
+    ['O20250915001', '2025/09/15', 'iERP｜智慧採購叫貨系統', '1', '待處理', '查看明細'],
+    ['O20250915002', '2025/09/14', 'iERP｜智慧採購叫貨系統', '3', '待處理', '查看明細'],
+    ['O20250915002', '2025/09/14', 'iERP｜智慧採購叫貨系統', '1', '待處理', '查看明細'],
+    ['O20250915002', '2025/09/14', 'iERP｜智慧採購叫貨系統', '3', '待處理', '查看明細'],
+    ['O20250915002', '2025/09/14', 'iERP｜智慧採購叫貨系統', '3', '待處理', '查看明細'],
+    ['O20250915002', '2025/09/14', 'iERP｜智慧採購叫貨系統', '3', '待處理', '查看明細'],
+    ['O20250915002', '2025/09/14', 'iERP｜智慧採購叫貨系統', '3', '待處理', '查看明細'],
+    ['O20250915002', '2025/09/14', 'iERP｜智慧採購叫貨系統', '3', '待處理', '查看明細'],
+    ['O20250915002', '2025/09/14', 'iERP｜智慧採購叫貨系統', '3', '待處理', '查看明細'],
+    ['O20250915002', '2025/09/14', 'iERP｜智慧採購叫貨系統', '3', '待處理', '查看明細']
+].flatMap((x) => ({
+    items: x.flatMap((y) => ({
+        text: y,
+        attribute: { style: { 'text-align': (!isNaN(y as unknown as number) || y === '待處理') ? 'center' : '' } },
+        component: y === '查看明細' ? TableLink : undefined,
+        data: y === '查看明細' ? { id: x[0] } : undefined
+    }))
+}));
+
+const width = {
+    詢問單號: '9rem',
+    詢問日期: '7.375rem',
+    產品名稱: 'auto',
+    數量: '4rem',
+    狀態: '5rem',
+    '': '6rem'
+};
+export const field: tableChidType[] = ['詢問單號', '詢問日期', '產品名稱', '數量', '狀態', ''].map((x) => ({
+    text: x,
+    attribute: { style: { width: width[x as keyof typeof width], 'text-align': ['數量', '狀態'].includes(x) ? 'center' : '' } }
+}));
